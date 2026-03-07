0c3f6270eb78008992c8c4d3c694932b210a2cad
Tom <tom@orbre.co.uk>
2026-03-07 10:13:45 +0000

    Initial Commit

---
+++ VinylScanner/VinylScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylScannerApp.swift
+//  VinylScanner
+//
+//  Created by Tom on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct VinylScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
